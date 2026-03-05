11e217d9b422c5d9c8869169dcf4f52b59ea54af
rochdi karouali <rochdi.karouali1234@gmail.com>
2026-03-05 22:28:32 +0000

    new update for pdf gls

--- resources/views/backoffice/certificates/pdf.blade.php
+++ resources/views/backoffice/certificates/pdf.blade.php
@@ -30,7 +30,7 @@
         }
 
         /* HEADER */
-        .header-logo { width: 100px; }
+        .header-logo { width: 140px; }
         .header-right {
             text-align: right;
             font-size: 10px;
@@ -159,6 +159,16 @@
             margin: 0 auto 5px auto;
         }
 
+        /* WATERMARK */
+        .watermark {
+            position: fixed;
+            top: 380px;
+            left: 175px;
+            width: 350px;
+            opacity: 0.06;
+            z-index: 0;
+        }
+
         /* FOOTER */
         .footer-address {
             position: fixed;
@@ -182,6 +192,9 @@
 </head>
 
 <body>
+
+    <!-- WATERMARK -->
+    <img src="{{ public_path('assets/images/logo/gls.png') }}" class="watermark">
 
     <!-- HEADER -->
     <table>
